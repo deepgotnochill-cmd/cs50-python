0a7113643fcada9ea20e8ed68606e4a1e75b78b3
deepgotnochill-cmd <dattachaudhurydeep17@gmail.com>
2026-09-10 23:21:33 +0530

    todays cpp practice

---
+++ Mathxstatsmodule.py
@@ -0,0 +1,11 @@
+import math
+import statistics
+data = [10,20,30,40,50]
+print("---Math Module---")
+print(f"Square root of 25: {math.sqrt(25)}")
+print(f"Factorial of 5:{math.factorial(5)}")
+print(f"Value of Pi : {math.pi}")
+print("\n---Statistics Module---")
+print(f"Mean:{statistics.mean(data)}")
+print(f"Median :{statistics.median(data)}")
+print(f"Mode:{statistics.mode(data)}")
